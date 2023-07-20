e14a67f4676ca9a8194a4deabae9d57d5223d7d3
botjuice <umayr.mikail@gmail.com>
2023-07-20 17:27:08 +0100

    completed 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(min, max) {
+    if ((min || max) < 0) return "ERROR";
+    else if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR"; 
+    let sum = 0;
+    let start = min;
+    let end = max;
+    if (min > max){
+        start = max;
+        end = min;
+    }
+    for (let num = start; num <= end; num++){
+        sum += num;
+    }
+    return sum;
 };
 
 // Do not edit below this line
