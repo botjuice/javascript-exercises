d1f2ad87e737dd8c7f070a2361bb1108df871fd4
botjuice <umayr.mikail@gmail.com>
2023-07-20 18:22:52 +0100

    completed 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+  let totalSum = 0;
+	for (let i = 0; i <= numbers.length - 1; i++){
+    totalSum += numbers[i];
+  }
+  return totalSum;
 };
 
 const multiply = function() {
-
+  var args = [];
+  for (var i = 0; i < arguments.length; ++i) args[i] = arguments[i];
+  let product = args[0];
+  for (let i = 1; i <= args.length - 1; i++){
+    product *= args[i];
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let answer = a;
+  if (a === 0) return 1;
+  for (let i = a - 1; i > 0; i--){
+    answer *= i;
+  }
+  return answer;
 };
 
 // Do not edit below this line
