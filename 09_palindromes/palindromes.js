99c30593ec219d8400fbe0dea8ff85c9862a538b
botjuice <umayr.mikail@gmail.com>
2023-08-24 09:26:00 +0300

    completed 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (text) {
+    let letters = "";
+    let reverseLetters = "";
+    for (let i = 0; i < text.length; i++) {
+        if (text[i].match(/^[A-Za-z0-9]*$/)) {
+            letters += text[i].toLowerCase();
+        }
+    }
 
+    for (let i = letters.length - 1; i >= 0; i--) {
+        reverseLetters += letters[i];
+    }
+    //return reverseLetters;
+    return reverseLetters === letters;
 };
 
 // Do not edit below this line
