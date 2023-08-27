fa86adc4aebb0fac8036c8ad9947866120c59693
botjuice <umayr.mikail@gmail.com>
2023-08-27 20:38:18 +0300

    completed 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num <= 0) return "OOPS";
+    let sequence = [1];
+    for (i = 0; i < num; i++) {
+        if (sequence.length >= 2) {
+            sequence.push((sequence[i-1]+sequence[i]));
+        }
+        else {
+            sequence.push(1);
+        }
+    }
+    return sequence[i-1];
 };
 
 // Do not edit below this line
