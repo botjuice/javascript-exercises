f3420e71086f1e82bdced737060dfe600e8cdabb
botjuice <umayr.mikail@gmail.com>
2023-09-17 15:23:59 +0300

    completed 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,21 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function (people) {
+    let ageInOrder = [];
+    let oldest = 0;
+    for (i = 0; i < people.length; i++) {
+        ageInOrder.push(getAge(people[i].yearOfBirth, people[i].yearOfDeath));
+    }
+    for (i = 0; i < ageInOrder.length; i++) {
+        if (ageInOrder[i] > oldest) { oldest = ageInOrder[i]; }
+    }
+    let oldestPerson = ageInOrder.indexOf(oldest);
+    return people[oldestPerson];
+}
+const getAge = function (birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
